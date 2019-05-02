e939dda96f8d33ec775137b9234bbdfdfab9c3e8
Seryas <seryas.ratised@gmail.com>
2019-05-02 23:57:51 +0100

    el commit li wra el merge .. w zedt fl sign up el calendar t3 birthdate

--- app/src/main/java/com/example/carpoolingappv1/SignupActivity.java
+++ app/src/main/java/com/example/carpoolingappv1/SignupActivity.java
@@ -1,12 +1,19 @@
 package com.example.carpoolingappv1;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,9 +21,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Calendar;
+
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     public EditText emailSignup,passwordSignup;
+
+    //date
+    public TextView bdateSignup;
+    private DatePickerDialog.OnDateSetListener bdateSetListener;
 
     private FirebaseAuth mAuth;
 
@@ -25,14 +38,53 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         findViewById(R.id.SignUpBtnId).setOnClickListener(this);
         findViewById(R.id.LogInTxtBtnId).setOnClickListener(this);
 
         emailSignup = findViewById(R.id.emailSignupId);
         passwordSignup = findViewById(R.id.passwordSignupId);
+        bdateSignup = (TextView) findViewById(R.id.birthdateSignupId);
 
 
-        mAuth = FirebaseAuth.getInstance();
+        //date displays widget
+        bdateSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SignupActivity.this,
+                        android.R.style.Theme_DeviceDefault_Dialog_NoActionBar_MinWidth,
+                        bdateSetListener,
+                        year,month,day);
+                //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+
+        //get the date and put it in the textField
+        bdateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                //
+                //Log.d("onDate set: mm/dd/yy" + month + "/" + dayOfMonth + "/" + year);
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                bdateSignup.setText(date);
+
+            }
+        };
+        //
+
+
     }
 
     @Override
