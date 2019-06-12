cf789359b52672d3c2082e58beff9363fd4d6bcc
Nix Malone <ztn.nizar@gmail.com>
2019-06-12 15:51:39 +0100

    Home recyclView V2

--- app/src/main/java/com/example/carpoolingappv1/CarteFragment.java
+++ app/src/main/java/com/example/carpoolingappv1/CarteFragment.java
@@ -33,12 +33,6 @@ public class CarteFragment extends Fragment implements OnMapReadyCallback {
     SupportMapFragment mapFragment;
 
 
-    /*@Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }*/
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
